9e3cf8724692c44844cfeb2c9f8b028120b68df1
xuye <15549228333@qq.com>
2018-09-12 14:23:37 +0800

    使用xcode新建c学习项目，运行测试

---
+++ ComputerSystemsLearning/ComputerSystemsLearning/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ComputerSystemsLearning
+//
+//  Created by xuye on 2018/9/12.
+//  Copyright © 2018年 xuye. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
